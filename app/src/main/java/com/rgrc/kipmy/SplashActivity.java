6cd59149c0b7362d9e70760863c360ca53258236
nomeRoseligamboa <emailroseligr26@gmail.com>
2021-08-24 05:40:17 -0300

    SplashScreen

---
+++ app/src/main/java/com/rgrc/kipmy/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.rgrc.kipmy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTheme(R.style.Theme_Splash);
+
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 5000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                SplashActivity.this,MainActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
